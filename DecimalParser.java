29d81d761430d96b331206f39ed24ba03bb65b28
Austin Schwartz <schwar12@purdue.edu>
2017-01-15 17:28:48 -0500

    Commit

--- DecimalParser.java
+++ DecimalParser.java
@@ -1,72 +1,64 @@
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 class DecimalParser {
-  public static void main(String args[]) throws Exception {
-	if (args.length==0) {
-          	System.out.println("Usage: java NumberParser value");
-		System.exit(1);
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            System.out.println("Usage: java NumberParser value");
+            System.exit(1);
         }
         double val = MyParseDecimal(args[0]);
-	System.out.println("Value="+val);
-  }
-
-  enum StateDecimal { SSTART, SINTEGER, SDECIMAL, SEND };
-
-  public static double MyParseDecimal(String s) throws Exception {
- 
-    StateDecimal state;
-
-    state = StateDecimal.SSTART;
-
-    int i = 0;
-    double divider = 10;
-    double value = 0;
-
-    while ( i < s.length() && state != StateDecimal.SEND) {
-	char ch = s.charAt(i);
-      	switch (state) {
-        	case SSTART: 
-			if (Character.isDigit(ch)) {
-				state = StateDecimal.SINTEGER;
-				value = Character.getNumericValue(ch);	
-				i++;
-			}
-			else {
-				throw new Exception("Bad format");
-			}
-			break;
-		case SINTEGER:
-			if (Character.isDigit(ch)) {
-                                state = StateDecimal.SINTEGER;
-                                value = 10.0*value + Character.getNumericValue(ch);
-                                i++;
-                        }
-			else if (ch == '.') {
-				state = StateDecimal.SDECIMAL;
-				i++;	
-			}
-			break;
-		case SDECIMAL:
-			if (Character.isDigit(ch)) {
-                                value = value + Character.getNumericValue(ch)/divider;
-				divider = divider * 10;
-                                i++;
-                        }
-			else {
-				throw new Exception("Bad format");
-			}
-			break;
-	}
-      
+        System.out.println("Value=" + val);
     }
 
-    return value;
+    enum StateDecimal {
+        SSTART, SINTEGER, SDECIMAL, SEND
+    }
 
+    public static double myParseDecimal(String str) throws Exception {
+        StateDecimal state = StateDecimal.SSTART;
 
-  }
+        int i = 0;
+        double divider = 10;
+        double value = 0;
 
+        while (i < str.length() && state != StateDecimal.SEND) {
+            char ch = str.charAt(i);
+            switch (state) {
+                case SSTART:
+                    if (Character.isDigit(ch)) {
+                        state = StateDecimal.SINTEGER;
+                        value = Character.getNumericValue(ch);
+                        i++;
+                    } else {
+                        throw new Exception("Bad format");
+                    }
+                    break;
+                case SINTEGER:
+                    if (Character.isDigit(ch)) {
+                        state = StateDecimal.SINTEGER;
+                        value = 10.0 * value + Character.getNumericValue(ch);
+                        i++;
+                    } else if (ch == '.') {
+                        state = StateDecimal.SDECIMAL;
+                        i++;
+                    }
+                    break;
+                case SDECIMAL:
+                    if (Character.isDigit(ch)) {
+                        value = value + Character.getNumericValue(ch) / divider;
+                        divider = divider * 10;
+                        i++;
+                    } else {
+                        throw new Exception("Bad format");
+                    }
+                    break;
+                default:
+                    throw new Exception("Bad format");
+            }
+        }
+        return value;
+    }
 }
 
 
